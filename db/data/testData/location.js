85387d5c6f203eaf14393e0586713d1d72ecd039
abigail-a-lee <aabigaillee@gmail.com>
2023-02-21 13:58:13 +0000

    added data

--- db/data/testData/location.js
+++ db/data/testData/location.js
@@ -1,14 +1,22 @@
 module.exports = [
   {
     locations: [[-0.07530110648445998, 51.50557305915922]],
+    img_url: "https://i.imgur.com/sM8Kz5Y.jpeg",
+    user_id: 1,
   },
   {
     locations: [[-0.0009108155069839086, 51.47733209786023]],
+    img_url: "https://i.imgur.com/podO8uW.jpeg",
+    user_id: 1,
   },
   {
     locations: [[-0.1429489005651874, 51.50080870807764]],
+    img_url: "https://i.imgur.com/dc8COUe.jpeg",
+    user_id: 2,
   },
   {
     locations: [[-0.15314146762585779, 51.534935924609954]],
+    img_url: "https://i.imgur.com/83jt46r.jpeg",
+    user_id: 4,
   },
 ];
