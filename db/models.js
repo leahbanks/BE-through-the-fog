9335d7312d33e00dfb54cca440826c8a1ee9bd9b
abigail-a-lee <aabigaillee@gmail.com>
2023-02-22 10:33:30 +0000

    fixed createUser model

--- db/models.js
+++ db/models.js
@@ -25,48 +25,51 @@ const fetchUsername = (query) => {
 };
 
 const createUser = (data) => {
+  const values = [data.username];
+  if (data.username === undefined) {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+  let sqlString = `INSERT INTO users (username`;
 
-    const values = [data.username]
-    if (post.username === undefined) {return Promise.reject({status : 400, msg : "Bad Request"})}
-    const sqlString = `INSERT INTO users (username`
+  if (data.password) {
+    (sqlString += `, password`), values.push(data.password);
+  }
+  if (data.avatar_url) {
+    (sqlString += `, avatar_url`), values.push(data.avatar_url);
+  }
 
-    if (data.password) {sqlString += `, password`, values.push(data.password)}
-    if (data.avatar_url) {sqlString += `, avatar_url`, values.push(data.avatar_url)}
+  sqlString += `)
+    VALUES ($1`;
 
-    sqlString += `) 
-    VALUES
-    ($1`
+  if (data.password) {
+    sqlString += `, $2`;
+  }
+  if (data.avatar_url) {
+    sqlString += `, $3`;
+  }
 
-    if (data.password) {sqlString += `, $2`}
-    if (data.avatar_url) {sqlString += `, $3`}
+  sqlString += `)
+    RETURNING *;`;
 
-    sqlString += `)
-    RETURNING *;`
-
-    return db.query(sqlString)
-    .then(({rows}) => rows)
-}
-
+  return db.query(sqlString, values).then(({ rows }) => rows);
+};
 
 const fetchGeoData = (user) => {
-    const values = [user]
+  const values = [user];
 
-    const checkUserSql = `SELECT * FROM users
-    WHERE users.user_id = $1;`
+  const checkUserSql = `SELECT * FROM users
+    WHERE users.user_id = $1;`;
 
-    const sqlString = `SELECT * FROM geodata
-    WHERE geodata.user_id = $1;`
+  const sqlString = `SELECT * FROM geodata
+    WHERE geodata.user_id = $1;`;
 
-    return db.query(checkUserSql, values).then((result) => {
-        if (result.rowCount === 0) {return Promise.reject({status: 404, msg: "User not found"})}
-        else {
-    
+  return db.query(checkUserSql, values).then((result) => {
+    if (result.rowCount === 0) {
+      return Promise.reject({ status: 404, msg: "User not found" });
+    } else {
+      return db.query(sqlString, values).then(({ rows }) => rows);
+    }
+  });
+};
 
-    return db.query(sqlString, values)
-        .then(({ rows }) => rows)}} 
-)
-}
-
-
-module.exports = {fetchUsername, fetchUsers, createUser, fetchGeoData};
-
+module.exports = { fetchUsername, fetchUsers, createUser, fetchGeoData };
