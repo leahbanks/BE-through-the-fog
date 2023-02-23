e586513372dfc5b953455786bb2f5e0c5d51e526
abigail-a-lee <aabigaillee@gmail.com>
2023-02-23 16:23:13 +0000

    added test data for trips

---
+++ db/data/testData/trips.js
@@ -0,0 +1,101 @@
+module.exports = [
+  {
+    location: `{ 0.13138741600173248, 51.56144203807303}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{0.13122676716699117, 51.56144529570609}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{0.131066118309234, 51.56144855311981}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{0.1743581, 51.5655201}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+
+  {
+    location: `{ 0.1309054694284627, 51.56145181031422}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{0.13074482052467862, 51.56145506728929}`,
+    trip_id: 1,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.1429489005651874, 51.50080870807764}`,
+    trip_id: 2,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.15314146762585779, 51.534935924609954}`,
+    trip_id: 2,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+
+  {
+    location: `{-0.07530110648445998, 51.50557305915922}`,
+    trip_id: 3,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.0009108155069839086, 51.47733209786023}`,
+    trip_id: 3,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.1429489005651874, 51.50080870807764}`,
+    trip_id: 3,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.15314146762585779, 51.534935924609954}`,
+    trip_id: 3,
+    user_id: 1,
+    circle_size: 0.5,
+  },
+
+  {
+    location: `{-0.07530110648445998, 51.50557305915922}`,
+    trip_id: 1,
+    user_id: 2,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.0009108155069839086, 51.47733209786023}`,
+    trip_id: 1,
+    user_id: 2,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.1429489005651874, 51.50080870807764}`,
+    trip_id: 1,
+    user_id: 2,
+    circle_size: 0.5,
+  },
+  {
+    location: `{-0.15314146762585779, 51.534935924609954}`,
+    trip_id: 1,
+    user_id: 2,
+    circle_size: 1.0,
+  },
+];
