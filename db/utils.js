ca28cf14c29fca732c66ca7ed19afcbc89b8acd3
abigail-a-lee <aabigaillee@gmail.com>
2023-02-24 11:29:21 +0000

    formatting trips utility added

--- db/utils.js
+++ db/utils.js
@@ -5,4 +5,35 @@ const Format_coords = (arr) => {
   return output;
 };
 
+const dummyData = [
+  {
+    entry_id: 7,
+    trip_id: 2,
+    user_id: 1,
+    location: [-0.1429489005651874, 51.50080870807764],
+    circle_size: 0.5,
+  },
+  {
+    entry_id: 8,
+    trip_id: 2,
+    user_id: 1,
+    location: [-0.15314146762585779, 51.534935924609954],
+    circle_size: 0.5,
+  },
+];
+
+const formatTrips = (payload) => {
+  const formattedResponse = {
+    user_id: payload[0].user_id,
+    trip_id: payload[0].trip_id,
+    points: payload.map((entry) => ({
+      coordinates: entry.location,
+      circleSize: entry.circle_size,
+    })),
+  };
+  return formattedResponse;
+};
+
+console.log(JSON.stringify(formatTrips(dummyData), null, 2));
+
 module.exports = { Format_coords };
